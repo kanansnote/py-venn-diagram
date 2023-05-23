f501d635e41733a2e481ede983f7d9a2460d333a
kanansnote <kanansnote@gmail.com>
2023-05-23 14:53:55 +0400

    Adding few more description to the labels

--- MyVennPy/Venn Diagram.py
+++ MyVennPy/Venn Diagram.py
@@ -8,17 +8,17 @@ import matplotlib.pyplot as plt
 from matplotlib_venn import venn3
 
 # Set the labels for each circle
-labels = ['Artistic & Philosophical', 'Technical', 'Financial & Healthy']
+labels = ['Artistic & Philosophical Interests', 'Technical & Nutritional Skills', 'Financial & Healthy Needs']
 
 # Create the Venn diagram with three circles
 venn_diagram = venn3(subsets=(1, 1, 1, 1, 1, 1, 1), set_labels=labels)
 
 # Add labels to the intersections of the circles
 venn_diagram.get_label_by_id('100').set_text('Fi')
-venn_diagram.get_label_by_id('010').set_text('Se')
+venn_diagram.get_label_by_id('010').set_text('Se & Ni')
 venn_diagram.get_label_by_id('001').set_text('Te')
 venn_diagram.get_label_by_id('110').set_text('Fe')
-venn_diagram.get_label_by_id('011').set_text('Ni')
+venn_diagram.get_label_by_id('011').set_text('Si')
 venn_diagram.get_label_by_id('101').set_text('Ti')
 venn_diagram.get_label_by_id('111').set_text('Ne')
 
